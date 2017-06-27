09488f744699a7d42ab672d571c2d6107cf2dcbf
Dhirendra Magadi <dmagadi@rsimail.com>
2017-06-27 16:26:40 -0700

    removed some comments from code

--- src/main/java/poc/cache/ParamResource.java
+++ src/main/java/poc/cache/ParamResource.java
@@ -11,25 +11,7 @@ import javax.ws.rs.QueryParam;
 import org.apache.commons.jcs.access.CacheAccess;
 import org.apache.commons.lang.StringUtils;
 
-/**
- * Resource that handles one way asynchronous chatting based on consumes
- * produces mechanism.
- * <p/>
- * Message is sent (produces) using POST http method handled by
- * {@link #postMessage(javax.ws.rs.container.AsyncResponse, String)} and this
- * message is received (consumed) by GET http method handled by {@link #getMessage(javax.ws.rs.container.AsyncResponse,
- * String)}.
- * <p/>
- * This implementation of produces consumes mechanism does not keep a queue of
- * messages but a queue of responses waiting for messages. Message is always
- * delivered directly from thread handling posting of message to the thread
- * handling receiving of message. Request processing must be therefore
- * synchronized by blocking threads. In order to save server resources the
- * original threads are returned to the container.
- *
- * @author Miroslav Fuksa (miroslav.fuksa at oracle.com)
- *
- */
+
 @Path("param")
 @Singleton
 public class ParamResource {
